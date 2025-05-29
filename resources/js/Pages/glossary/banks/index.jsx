4052ea464ece2dbf61412faa9b36fc60b9f720fb
DavidKaigorodov <bossdilan2003@gmail.com>
2025-05-29 11:05:39 +0700

    Работаю нал таблицей

--- resources/js/Pages/glossary/banks/index.jsx
+++ resources/js/Pages/glossary/banks/index.jsx
@@ -1,10 +1,11 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import TableComponent from "../../../components/table/Table";
 
 export default function index({banks}) {
     console.log(banks);
     return (
         <AuthenticatedLayout>
-            <p>test</p>
+            <TableComponent />
         </AuthenticatedLayout>
     );
 }
