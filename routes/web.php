3211a685763942ba8dd5ae42c5bc92c8678be4bd
47Mags47 <gord_ka47@mail.ru>
2025-06-02 14:33:45 +0700

    Удалил FtpController

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::group([], [
     base_path('routes/web/docs.php'),
     base_path('routes/web/auth.php'),
     base_path('routes/web/glossary.php'),
-    base_path('routes/web/ftp.php'),
     base_path('routes/web/payment.php'),
     base_path('routes/web/validate.php'),
 ]);
--- routes/web/ftp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Web\FtpController;
-
-Route::middleware('auth')->prefix('/ftp/files')->name('ftp.files.')->controller(FtpController::class)->group(function () {
-    Route::get('/modal', 'modal')->name('modal');
-    Route::get('/table', 'table')->name('table');
-});
