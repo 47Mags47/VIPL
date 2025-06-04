91510da06fa9e0d890414424b6f62491d1e1d2a9
47Mags47 <gord_ka47@mail.ru>
2025-06-04 15:39:39 +0700

    Завершил

---
+++ app/Exporters/ATBBankExporter.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Exporters;
+
+use App\Classes\ExcelExporter;
+use Illuminate\Support\Facades\Storage;
+
+class ATBBankExporter extends ExcelExporter
+{
+    public function __construct()
+    {
+        parent::__construct(...func_get_args());
+        $this->setFileName('sp22_' . substr($this->raport_npp, 3, 2) . '.xls');
+
+        $this->spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load(Storage::disk('local')->path('templates/payment_raport_atb.xls'));
+    }
+
+    public function save(): ATBBankExporter
+    {
+        $this->spreadsheet->getActiveSheet()
+            ->setCellValue('A1', 'Реестр на перечисление социальных выплат за ' . $this->event->date->translatedFormat('F Y') . 'г.')
+            ->setCellValue('B2', 'на счета физических лиц от ('. $this->bank->contract->division->name .')')
+            ->setCellValue('A3', $this->event->date->format('d.m.Y'));
+
+        $data_array = $this->recipients->map(function ($recipient, $i) {
+            return [
+                $i + 1,
+                mb_strtoupper((string) $recipient->last_name) . ' ' . mb_strtoupper((string) $recipient->first_name) . ' ' . mb_strtoupper((string) $recipient->middle_name),
+                (string) $recipient->account,
+                (string) number_format($recipient->summ, 2, '.', ''),
+            ];
+        });
+        $this->spreadsheet->getSheetByName('Реестр')->fromArray($data_array->toArray(), NULL, 'A5');
+
+        $highestRow = $this->spreadsheet->getActiveSheet()->getHighestRow();
+        $this->spreadsheet->getActiveSheet()->setCellValue([1, $highestRow + 1], 'ИТОГО:');
+        $this->spreadsheet->getActiveSheet()->setCellValue([4, $highestRow + 1], number_format($this->recipients->sum('summ'), 2, '.', ''));
+
+        foreach (range('A', 'D') as $col) {
+            $this->spreadsheet->getActiveSheet()
+                ->getColumnDimension($col)
+                ->setAutoSize(true);
+        }
+
+        $this->writer = new \PhpOffice\PhpSpreadsheet\Writer\Xls($this->spreadsheet);
+        $this->writer->save($this->getFullPath());
+
+        return $this;
+    }
+}
--- database/seeders/Glossary/BankSeeder.php
+++ database/seeders/Glossary/BankSeeder.php
@@ -25,24 +25,24 @@ class BankSeeder extends Seeder
         BankExporter::create(['code' => 'levobereg',    'name' => 'Левобережный']);
         BankExporter::create(['code' => 'alfa',         'name' => 'Альфа']);
         BankExporter::create(['code' => 'sovkom',       'name' => 'Совком']);
+        BankExporter::create(['code' => 'ATB',          'name' => 'АТБ']);
 
-        Bank::create(['number_code' => '014',   'code' => 'Газпромбан',   'name' => 'АО "Газпромбанк"',                           'exporter_id' => BankExporter::byCode('other')->id]);
-        Bank::create(['number_code' => '016',   'code' => 'Промсвязьб',   'name' => 'ПАО "Промсвязьбанк"',                        'exporter_id' => BankExporter::byCode('other')->id]);
-        Bank::create(['number_code' => '022',   'code' => 'АТБ',          'name' => 'Азиатско Тихоокеанский Банк (ПАО)',          'exporter_id' => BankExporter::byCode('other')->id]);
-        Bank::create(['number_code' => '002',   'code' => 'СБРФ8615',     'name' => 'ПАО "Сбербанк России"',                      'exporter_id' => BankExporter::byCode('sber')->id]);
-        Bank::create(['number_code' => '010',   'code' => 'Сбер_Нерез',   'name' => 'ПАО "Сбербанк России" (нерезиденты)',        'exporter_id' => BankExporter::byCode('sber')->id]);
-        Bank::create(['number_code' => '011',   'code' => 'Сбер_номин',   'name' => 'ПАО "Сбербанк России" (номинальные счета)',  'exporter_id' => BankExporter::byCode('sber')->id]);
-        Bank::create(['number_code' => '013',   'code' => 'СБРФне8615',   'name' => 'ПАО "Сбербанк России" (не 8615)',            'exporter_id' => BankExporter::byCode('sber')->id]);
-        Bank::create(['number_code' => '005',   'code' => 'Уралсиб',      'name' => 'ПАО "Банк Уралсиб"',                         'exporter_id' => BankExporter::byCode('UralSib')->id]);
-        Bank::create(['number_code' => '007',   'code' => 'Уралс_карт',   'name' => 'ПАО "Банк Уралсиб" (карточка)',              'exporter_id' => BankExporter::byCode('UralSib')->id]);
-        Bank::create(['number_code' => '006',   'code' => 'Россельхоз',   'name' => 'АО "Россельхозбанк"',                        'exporter_id' => BankExporter::byCode('RosSelhoz')->id]);
-        Bank::create(['number_code' => '012',   'code' => 'Россельнер',   'name' => 'АО "Россельхозбанк" (нерезиденты)',          'exporter_id' => BankExporter::byCode('RosSelhoz')->id]);
-        Bank::create(['number_code' => '015',   'code' => 'Почта  банк',  'name' => 'ПАО "Почта банк"',                           'exporter_id' => BankExporter::byCode('pochta')->id]);
-        Bank::create(['number_code' => '017',   'code' => 'Банк ВТБ',     'name' => 'ПАО "Банк ВТБ"',                             'exporter_id' => BankExporter::byCode('vtb')->id]);
-        Bank::create(['number_code' => '020',   'code' => 'КББ',          'name' => 'КузнецкБизнесБанк',                          'exporter_id' => BankExporter::byCode('kbb')->id]);
-        Bank::create(['number_code' => '021',   'code' => 'ЛЕВОБЕРЕЖ',    'name' => 'ПАО БАНК "ЛЕВОБЕРЕЖНЫЙ"',                    'exporter_id' => BankExporter::byCode('levobereg')->id]);
-        Bank::create(['number_code' => '023',   'code' => 'Альфа-Банк',   'name' => 'АО "Альфа-Банк"',                            'exporter_id' => BankExporter::byCode('alfa')->id]);
-        Bank::create(['number_code' => '024',   'code' => 'Совкомбанк',   'name' => 'ПАО "Совкомбанк"',                           'exporter_id' => BankExporter::byCode('sovkom')->id]);
-
+        Bank::create(['number_code' => '002',   'code' => 'СБРФ8615',       'name' => 'ПАО "Сбербанк России"',                      'exporter_id' => BankExporter::byCode('sber')->id]);
+        Bank::create(['number_code' => '005',   'code' => 'Уралсиб',        'name' => 'ПАО "Банк Уралсиб"',                         'exporter_id' => BankExporter::byCode('UralSib')->id]);
+        Bank::create(['number_code' => '006',   'code' => 'Россельхоз',     'name' => 'АО "Россельхозбанк"',                        'exporter_id' => BankExporter::byCode('RosSelhoz')->id]);
+        Bank::create(['number_code' => '007',   'code' => 'Уралс_карт',     'name' => 'ПАО "Банк Уралсиб" (карточка)',              'exporter_id' => BankExporter::byCode('UralSib')->id]);
+        Bank::create(['number_code' => '010',   'code' => 'Сбер_Нерез',     'name' => 'ПАО "Сбербанк России" (нерезиденты)',        'exporter_id' => BankExporter::byCode('sber')->id]);
+        Bank::create(['number_code' => '011',   'code' => 'Сбер_номин',     'name' => 'ПАО "Сбербанк России" (номинальные счета)',  'exporter_id' => BankExporter::byCode('sber')->id]);
+        Bank::create(['number_code' => '012',   'code' => 'Россельнер',     'name' => 'АО "Россельхозбанк" (нерезиденты)',          'exporter_id' => BankExporter::byCode('RosSelhoz')->id]);
+        Bank::create(['number_code' => '013',   'code' => 'СБРФне8615',     'name' => 'ПАО "Сбербанк России" (не 8615)',            'exporter_id' => BankExporter::byCode('sber')->id]);
+        Bank::create(['number_code' => '014',   'code' => 'Газпромбан',     'name' => 'АО "Газпромбанк"',                           'exporter_id' => BankExporter::byCode('other')->id]);
+        Bank::create(['number_code' => '015',   'code' => 'Почта  банк',    'name' => 'ПАО "Почта банк"',                           'exporter_id' => BankExporter::byCode('pochta')->id]);
+        Bank::create(['number_code' => '016',   'code' => 'Промсвязьб',     'name' => 'ПАО "Промсвязьбанк"',                        'exporter_id' => BankExporter::byCode('other')->id]);
+        Bank::create(['number_code' => '017',   'code' => 'Банк ВТБ',       'name' => 'ПАО "Банк ВТБ"',                             'exporter_id' => BankExporter::byCode('vtb')->id]);
+        Bank::create(['number_code' => '020',   'code' => 'КББ',            'name' => 'КузнецкБизнесБанк',                          'exporter_id' => BankExporter::byCode('kbb')->id]);
+        Bank::create(['number_code' => '021',   'code' => 'ЛЕВОБЕРЕЖ',      'name' => 'ПАО БАНК "ЛЕВОБЕРЕЖНЫЙ"',                    'exporter_id' => BankExporter::byCode('levobereg')->id]);
+        Bank::create(['number_code' => '022',   'code' => 'АТБ',            'name' => 'Азиатско Тихокеанский Банк (ПАО)',           'exporter_id' => BankExporter::byCode('ATB')->id]);
+        Bank::create(['number_code' => '023',   'code' => 'Альфа-Банк',     'name' => 'АО "Альфа-Банк"',                            'exporter_id' => BankExporter::byCode('alfa')->id]);
+        Bank::create(['number_code' => '024',   'code' => 'Совкомбанк',     'name' => 'ПАО "Совкомбанк"',                           'exporter_id' => BankExporter::byCode('sovkom')->id]);
     }
 }
