1c68babdfb1301671a261225fc4af60864fd5e85
47Mags47 <gord_ka47@mail.ru>
2025-06-04 12:56:36 +0700

    Порправил glossary.payments

--- app/Http/Controllers/Web/Glossary/PaymentController.php
+++ app/Http/Controllers/Web/Glossary/PaymentController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Glossary\Payment\StoreRequest;
 use App\Http\Requests\Glossary\Payment\UpdateRequest;
 use App\Models\Glossary\Law;
 use App\Models\Glossary\Payment;
+use App\Models\Glossary\PaymentPeriodicity;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -19,7 +20,7 @@ class PaymentController extends Controller
         $laws = Law::paginate(50)->toResourceCollection();
         $periodicityes = PaymentPeriodicity::paginate(50)->toResourceCollection();
 
-        return Inertia::render('glossary/payments/index', compact('payments'));
+        return Inertia::render('glossary/payments/index', compact('payments', 'laws', 'periodicityes'));
     }
 
     public function store(StoreRequest $request)
--- routes/web/glossary.php
+++ routes/web/glossary.php
@@ -42,36 +42,6 @@ Route::middleware('auth')->prefix('/glossary')->name('glossary.')->group(functio
         Route::get('/index', 'index')->name('index');
         Route::post('/store', 'store')->name('store');
         Route::prefix('/{payment}')->group(function () {
-            Route::get('/edit', 'edit')->name('edit');
-            Route::put('/update', 'update')->name('update');
-            Route::delete('/delete', 'delete')->name('delete');
-        });
-    });
-
-    Route::prefix('/banks')->controller(BankController::class)->name('banks.')->group(function () {
-        Route::get('/index', 'index')->name('index');
-        Route::post('/store', 'store')->name('store');
-        Route::prefix('/{bank}')->group(function () {
-            Route::get('/edit', 'edit')->name('edit');
-            Route::delete('/delete', 'delete')->name('delete');
-        });
-    });
-
-    Route::prefix('/divisions')->controller(DivisionController::class)->name('divisions.')->group(function () {
-        Route::get('/index', 'index')->name('index');
-        Route::post('/store', 'store')->name('store');
-        Route::prefix('/{division}')->group(function () {
-            Route::put('/update', 'update')->name('update');
-            Route::delete('/delete', 'delete')->name('delete');
-        });
-    });
-
-    Route::prefix('/payments')->controller(PaymentController::class)->name('payments.')->group(function () {
-        Route::get('/index', 'index')->name('index');
-        Route::get('/create', 'create')->name('create');
-        Route::post('/store', 'store')->name('store');
-        Route::prefix('/{payment}')->group(function () {
-            Route::get('/edit', 'edit')->name('edit');
             Route::put('/update', 'update')->name('update');
             Route::delete('/delete', 'delete')->name('delete');
         });
