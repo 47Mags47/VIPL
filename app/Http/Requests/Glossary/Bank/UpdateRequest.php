b8170df2275183e2ed94c07e8ef61dfa5226735c
47Mags47 <gord_ka47@mail.ru>
2025-06-09 09:02:02 +0700

    fix

--- app/Http/Requests/Glossary/Bank/UpdateRequest.php
+++ app/Http/Requests/Glossary/Bank/UpdateRequest.php
@@ -25,18 +25,15 @@ class UpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'bank'                        => ['array:number_code,code,name,exporter_id'],
             'bank.number_code'            => ['required', 'string', 'min:3', 'max:3', 'unique:' . Bank::class . ',number_code,' . $this->route('bank')->id],
             'bank.code'                   => ['required', 'string', 'min:3', 'max:10', 'unique:' . Bank::class . ',name,' . $this->route('bank')->id],
             'bank.name'                   => ['required', 'string', 'min:3', 'max:255'],
             'bank.exporter_id'            => ['required', 'exists:' . BankExporter::getTableName() . ',id'],
 
-            'contract'                    => ['array:number,signed_at,division_side_id'],
             'contract.number'             => ['required', 'string', 'max:255'],
             'contract.signed_at'          => ['required', 'date'],
             'contract.division_side_id'   => ['required', 'exists:' . ContractSide::getTableName() . ',id'],
 
-            'bank_side'                   => ['array:name,INN,account,BIK,comment'],
             'bank_side.name'              => ['required', 'string', 'max:255'],
             'bank_side.INN'               => ['required', 'string', 'min:10', 'max:10'],
             'bank_side.account'           => ['required', 'string', 'min:20', 'max:20'],
--- resources/js/includes/Flash.jsx
+++ resources/js/includes/Flash.jsx
@@ -1,8 +1,12 @@
 import { usePage } from "@inertiajs/react";
-
+import { useEffect } from "react";
 
 export default function Flash() {
     const messages = usePage().props.flash
+
+    useEffect(() => {
+        openFlash()
+    }, [messages])
 
     return (
         <div className="flash-message-box">
