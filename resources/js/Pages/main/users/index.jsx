e465eeab51df9fb24b2f97a0246e7c6dbc1f210c
DavidKaigorodov <bossdilan2003@gmail.com>
2025-06-20 11:39:18 +0700

    Merge commit '8b3a9904fd9a6990b85ece6b98885caab326595b'

--- resources/js/Pages/main/users/index.jsx
+++ resources/js/Pages/main/users/index.jsx
@@ -2,48 +2,71 @@ import { usePage } from "@inertiajs/react";
 
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
+import { CheckCircleTwoTone, CloseCircleTwoTone } from '@ant-design/icons';
+
 import Table from "@/components/Table";
 
-import Edit from "./Edit";
-import Create from "./Create";
-import Delete from "./Delete";
+// import Edit from "./Edit";
+// import Create from "./Create";
+// import Delete from "./Delete";
 
 
 export default function index() {
-    const laws = usePage().props.laws.data
-    const sources = usePage().props.sources.data
+    const user = usePage().props.user.data
+    const users = usePage().props.users.data
+    console.log(users);
 
 
     const columns = [
         {
-            title: ' Код',
-            dataIndex: 'code',
+            title: 'Онлайн',
+            dataIndex: 'online',
             width: 80,
+            render: (_, record) =>
+                record.online ? (
+                    <CheckCircleTwoTone twoToneColor="#52c41a" />
+                ) : (
+                    <CloseCircleTwoTone twoToneColor="#ff4d4f" />
+                ),
         },
         {
-            title: 'Наименование',
+            title: 'Имя',
             dataIndex: 'name',
         },
         {
-            title: 'Вид финансирования',
-            dataIndex: ['source', 'name'],
+            title: 'Email',
+            dataIndex: 'email',
             width: 150,
+        },
+        {
+            title: 'Подразделение',
+            dataIndex: ['division', 'name'],
+            width: 150,
+        },
+        {
+            title: 'Роли',
+            dataIndex: ['roles', 'name'],
+            width: 150,
+            render: (_, record) => {
+                const roleNames = record.roles?.map(role => role.name).join(', ') || '—';
+                return <span>{roleNames}</span>;
+            }
         },
         {
             title: '',
             key: 'edit',
             width: 80,
-            render: (_, record) => (
-                <Edit sources={sources} record={record} />
-            )
+            // render: (_, record) => (
+            //     <Edit sources={sources} record={record} />
+            // )
         },
         {
             title: '',
             key: 'delete',
             width: 80,
-            render: (_, record) => (
-                <Delete record={record} />
-            )
+            // render: (_, record) => (
+            //     <Delete record={record} />
+            // )
         },
     ];
 
@@ -52,10 +75,10 @@ export default function index() {
             <Table
                 rowKey="id"
                 columns={columns}
-                dataSource={laws}
-                actions={
-                    <Create sources={sources} />
-                }
+                dataSource={users}
+            // actions={
+            //     // <Create sources={sources} />
+            // }
             />
         </AuthenticatedLayout>
     );
