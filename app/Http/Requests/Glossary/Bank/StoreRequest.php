06bb541764437ca8400f6b1602f576c813352822
DavidKaigorodov <bossdilan2003@gmail.com>
2025-06-25 08:39:01 +0700

    back

--- app/Http/Requests/Glossary/Bank/StoreRequest.php
+++ app/Http/Requests/Glossary/Bank/StoreRequest.php
@@ -14,7 +14,7 @@ class StoreRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return user()->hasPermission('glossary-bank-create');
+        return true;
     }
 
     /**
