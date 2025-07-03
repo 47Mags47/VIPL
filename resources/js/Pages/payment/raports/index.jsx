de0ce75f091152b04909763bccc9690e71f4bcfa
DavidKaigorodov <bossdilan2003@gmail.com>
2025-07-03 13:08:51 +0700

    завершил

--- resources/js/Pages/payment/raports/index.jsx
+++ resources/js/Pages/payment/raports/index.jsx
@@ -1,23 +1,63 @@
-import { usePage, router, Link } from "@inertiajs/react";
+import { usePage, router } from "@inertiajs/react";
 
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
-import BlueButton from '@/components/button/BlueButton';
-import Table from "@/components/Table";
+import { Table, GoToButton, DeleteButton } from "@/components/table";
+import BlueButton from '@/components/buttons/BlueButton';
 
 
 export default function Index() {
-    const event = usePage().props.event.data
+    const { raports, event } = usePage().props
 
-    function GenerateRaport(e){
-        router.post(route('payments.raports.store', {event: event.id}))
+    const columns = [
+        {
+            title: 'id',
+            dataIndex: 'id',
+        },
+        {
+            title: ' Наименование',
+            dataIndex: 'original_name',
+        },
+        {
+            title: 'Кто начал',
+            dataIndex: 'start_by',
+        },
+        {
+            title: 'Создан',
+            dataIndex: 'created_at',
+            render: (value) => new Date(value).toLocaleDateString()
+        },
+        {
+            title: '',
+            key: 'show',
+            width: 80,
+            render: (_, record) => (
+                <GoToButton href={route('payments.raports.show', { raport: record.id })} />
+            )
+        },
+        {
+            key: 'delete',
+            width: 80,
+            render: (_, record) => (
+                <DeleteButton href={route('payments.raports.destroy', { raport: record.id })} />
+            )
+        },
+    ]
+
+    function raportGenerate(){
+        router.post(route('payments.raports.store', { event: event.data.id }))
     }
 
     return (
         <AuthenticatedLayout>
             <Table
+                rowKey="id"
+                columns={columns}
+                data={raports}
                 actions={
-                    <BlueButton onClick={GenerateRaport}>Сформировать</BlueButton>
+                    <BlueButton onClick={raportGenerate}>
+                        Сформировать отчет
+                    </BlueButton>
                 }
             />
         </AuthenticatedLayout >
