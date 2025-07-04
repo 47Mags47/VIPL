904289a4632e877b59c83ace50dc08017f224980
DavidKaigorodov <bossdilan2003@gmail.com>
2025-07-04 16:39:29 +0700

    завершил

--- resources/js/Pages/glossary/validator/Edit.jsx
+++ resources/js/Pages/glossary/validator/Edit.jsx
@@ -4,6 +4,7 @@ import { AuthenticatedLayout as Layout } from '@/layouts';
 import { VerticalForm as Form, StringInput as Input, Select } from '@/components/forms';
 
 import List from '@/components/forms/inputs/List';
+import { useEffect } from 'react';
 
 export default function Edit() {
     const column = usePage().props.column.data
@@ -61,9 +62,11 @@ export default function Edit() {
                 />
                 <List
                     items={data.patterns}
-                    setItems={(newPatterns) => setData('patterns', newPatterns)}
+                    setItems={(value) => {
+                        setData('patterns', value)}}
                     name="patterns"
                     label="Шаблоны"
+                    // added={false}
                 />
             </Form>
         </Layout>
---
+++ resources/js/components/forms/inputs/EditableText.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState, useRef } from 'react'
+
+import Error from '@/components/Error'
+import { StringInput as Input } from '@/components/forms'
+
+
+export default function EditableText({ ...props }) {
+    const value = props.value ?? ''
+    const index = props.index
+    const blurHandler = props.blurHandler
+    const onChange = props.handleChange
+    const name = props.name
+    const hasFocus = props.hasFocus
+
+    const [editing, setEditing] = useState(true)
+    const firstUpdate = useRef(true);
+
+    const onBlur = (e) => {
+        if (e.target.value.trim() == '')
+            blurHandler(e, index)
+
+        setEditing(false)
+    }
+
+    const spanRender = () => (
+        <div className="label-list span">
+            <span>{value || '...'}</span>
+            <Error name={`${name}.${index}`} />
+        </div>
+    )
+
+    const inputRender = () => (
+        <Input
+            name={`${name}[${index}]`}
+            value={value}
+            onChange={(e) => onChange(e, index)}
+            onBlur={onBlur}
+            placeholder="Введите значение"
+            autoFocus
+        />
+    )
+
+    const render = () => editing ? inputRender() : spanRender()
+
+    useEffect(() => {
+        if (firstUpdate.current && hasFocus) {
+            firstUpdate.current = false;
+            setEditing(false)
+            return
+        }
+    }, [editing])
+
+    return (
+        <div
+            className="list-item"
+            onDoubleClick={() => { setEditing(true) }}
+        >
+            <div className="left">
+                {render()}
+            </div>
+        </div>
+    )
+}
