6d6371f2839bca9e0ecd54cf98d9701fb18589ef
DavidKaigorodov <bossdilan2003@gmail.com>
2025-07-07 08:52:21 +0700

    Merge commit 'c67bc35b5cf36051f6c81453b5793d02609723af'

--- resources/js/components/forms/index.js
+++ resources/js/components/forms/index.js
@@ -3,3 +3,6 @@ export { default as VerticalForm } from './VerticalForm'
 export { default as StringInput } from './inputs/StringInput'
 export { default as Select } from './inputs/Select'
 export { default as TextArea } from './inputs/TextArea'
+export { default as List } from './inputs/List'
+export { default as EditableText } from './inputs/EditableText'
+
