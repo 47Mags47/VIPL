ca297bf161478f46fc70c7902a8c9570ff2506cd
47Mags47 <gord_ka47@mail.ru>
2025-07-16 15:24:07 +0700

    fix

--- resources/js/components/forms/inputs/DatePicker.jsx
+++ resources/js/components/forms/inputs/DatePicker.jsx
@@ -8,32 +8,45 @@ import { DatePicker as Date } from 'antd'
 export default function DatePicker({ ...props }) {
     const name = props.name
     const label = props.label
-    const value = props.value !== ''
-        ? dayjs(props.value, 'YYYY-MM-DD HH:mm:ss')
-        : ''
-
+    const multiple = props.multiple ?? false
+    const value = toDate(props.value)
     const onChange = props.onChange
     const format = props.format ?? 'DD.MM.YYYY'
 
-    function changeHandler(date, str) {
-        onChange(date.format('YYYY-MM-DD HH:mm:ss'))
+    function toString(date) {
+        return Array.isArray(props.value)
+            ? date.map((day) => day.format('YYYY-MM-DD HH:mm:ss'))
+            : date.format('YYYY-MM-DD HH:mm:ss')
     }
+
+    function toDate(date) {
+        return Array.isArray(props.value)
+            ? date.map((day) => dayjs(day, 'YYYY-MM-DD HH:mm:ss'))
+            : dayjs(date, 'YYYY-MM-DD HH:mm:ss')
+    }
+
+    function changeHandler(date) {
+        onChange(toString(date ?? []))
+    }
+
 
     return (
         <Label name={name} label={label} >
             <Date
-                classNames={{
-                    root: 'date-picker',
-                    popup: { root: 'date-picker-popup' }
-                }}
                 value={value}
                 onChange={changeHandler}
                 locale={locale.Calendar}
                 placeholder={format.replace(/[a-zA-Z]/g, '_')}
+                multiple={multiple}
+                classNames={{
+                    root: 'date-picker',
+                    popup: { root: 'date-picker-popup' }
+                }}
                 format={{
                     format: format,
                     type: 'mask',
                 }}
+
             />
         </Label>
     )
