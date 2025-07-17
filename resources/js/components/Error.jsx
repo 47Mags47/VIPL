ab00108a0c335fb85e5048fd6113fbd1455e7712
DavidKaigorodov <bossdilan2003@gmail.com>
2025-07-17 10:55:24 +0700

    добавил стилей для ошибок

--- resources/js/components/Error.jsx
+++ resources/js/components/Error.jsx
@@ -3,12 +3,13 @@ import { usePage } from "@inertiajs/react"
 export default function Error({ name }) {
     const { errors } = usePage().props
 
+    if (name.includes('['))
+        name = name.replaceAll('[', '.').replaceAll(']', '')
+
     let reg = new RegExp(`(^${name}$)|(^${name}\.[0-9]{1,99}$)|(^${name}.*\.[0-9]{1,99}$)`)
     let keys = Object.keys(errors).filter((item) => reg.test(item))
 
-    // DELETE
-    // if (name.includes('['))
-    //     name = name.replaceAll('[', '.').replaceAll(']', '')
+
 
     return (
         <ul className="form-errors">
