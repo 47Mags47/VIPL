89d47d8286bf7e9f7f82a21f52a4313888113c3f
47Mags47 <gord_ka47@mail.ru>
2025-07-17 10:00:42 +0700

    Переписал логику компонента Error

--- resources/js/components/Error.jsx
+++ resources/js/components/Error.jsx
@@ -3,12 +3,18 @@ import { usePage } from "@inertiajs/react"
 export default function Error({ name }) {
     const { errors } = usePage().props
 
-    if(name.includes('['))
-        name = name.replaceAll('[', '.').replaceAll(']', '')
+    let reg = new RegExp(`(^${name}$)|(^${name}\.[0-9]{1,99}$)|(^${name}.*\.[0-9]{1,99}$)`)
+    let keys = Object.keys(errors).filter((item) => reg.test(item))
+
+    // DELETE
+    // if (name.includes('['))
+    //     name = name.replaceAll('[', '.').replaceAll(']', '')
 
     return (
-        <div className="form-errors">
-            {errors[name] && <div>{errors[name]}</div>}
-        </div>
+        <ul className="form-errors">
+            {keys.map((key, i) => (
+                <li key={i}>{errors[key]}</li>
+            ))}
+        </ul>
     )
 }
